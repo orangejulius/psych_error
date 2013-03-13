28c00be9695f4873822472e874a8567a4efee18c
Julian Simioni <julian.simioni@gmail.com>
2013-03-13 14:26:46 -0700

    Simple project that demonstrates psych error

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PsychTest::Application.config.secret_token = 'b971773aadb35c314dcabb78c85cc9a276b7b51d93bf73a3dcbe18ed8311d7501eff7c0b0b5ca5592df24880535fb3b1f14330c4578b0bed8aebecf352a3cb12'
